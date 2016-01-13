6665b9e1e1934f605de89ad633d0b31fdcb345f6
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-13 15:02:35 -0500

    Exercise5.rb commit, formula for conversion now works using floating numbers but I can't get the gets chomp to call the method so the program stops before computing F to C.

--- exercise5.rb
+++ exercise5.rb
@@ -1,11 +1,11 @@
-# puts "Let's convert Fº to Cº! Please give me a temperature in Fahrenheit:"
-#
-# f = gets.chomp.to_i
+puts "Let's convert Fº to Cº! Please give me a temperature in Fahrenheit:"
+
+f = gets.chomp
 
 #I think it's going to break here because I have no way of calling the method f_to_c after I've asked the user for input...
 
 def f_to_c(f)
-  c = ((f.to_i - 32) * (5/9))
-  puts "The Fahrenheit equivalent of #{f_temp} is #{c} degrees Celcius."
+  c = (f - 32) * (5.0 / 9.0)
+  puts "The Fahrenheit equivalent of #{f} is #{c} degrees Celcius."
   return c
 end
