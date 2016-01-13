46f93dde3e866933d90f5b004518baf15e36f9b8
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-13 17:49:35 -0500

    ex7.rb Killed cohort 2 and displayed updated hash.

--- exercise7.rb
+++ exercise7.rb
@@ -23,3 +23,9 @@ students[:cohort4] = 43
 puts "Classrooms have grown by 5%; we have new cohort sizes!"
 
 cohort_plus_five_percent(students)
+
+puts "The second cohort disappeared into the space-time continuum. Here's the new list!"
+
+#Let's call the method while deleting :cohort2 in one line.
+students.delete(:cohort2)
+cohort_name_number(students)
