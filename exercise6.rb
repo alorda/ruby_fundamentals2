cb244e7e8e2f671c0744d6331b1f90ab1f3314e4
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-13 16:18:07 -0500

    Exercise6.rb, figured out how to put the each iterr into a method and invoke it.

--- exercise6.rb
+++ exercise6.rb
@@ -1,3 +1,14 @@
+def show_me_groceries(list)
+  list.each { |item| puts item }
+  puts "\n"
+end
+
 grocery_list = ["carrots", "celery", "humus", "chocolate", "peanuts"]
 
 grocery_list.each { |item| puts item }
+
+puts "I need to add some more items... let's check the list again:\n"
+
+grocery_list << "rice"
+
+show_me_groceries(grocery_list)
