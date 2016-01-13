f5ac59b1f7160e87e8042c568911481244f94998
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-13 18:59:10 -0500

    Reorganized exercise7.rb with comments and rewriting methods.

--- exercise7.rb
+++ exercise7.rb
@@ -1,10 +1,13 @@
+#This is my hash
+
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
   :cohort3 => 22
 }
 
-def cohort_name_number(hash)
+#These are all my methods:
+def display_cohort(hash)
   hash.each do |cohort, number|
     puts "#{cohort.capitalize} had #{number} students."
   end
@@ -16,25 +19,36 @@ def cohort_plus_five_percent(hash)
   end
 end
 
-def add_all_cohorts(hash)
+def add_a_cohort(hash, key, value)
+  hash[key.to_sym] = value
+end
+
+def sum_all_cohorts (hash)
   total_students = hash.each_value.inject(:+)
   puts total_students
 end
 
-cohort_name_number(students)
+#End of Method Definitions: Let's use them!
 
-students[:cohort4] = 43
+#Step 2
+display_cohort(students)
 
+#Step 3 Add cohort 4 with 43 students
+
+#Step 4 - Use Keys method to output cohort names
+puts "These are the names of the cohorts:"
+puts students.keys 
+
+
+#Step 5 - Expand Classrooms by 5% - Display - Done
 puts "Classrooms have grown by 5%; we have new cohort sizes!"
 
 cohort_plus_five_percent(students)
 
+#Step 6 Delete the second cohort and display
 puts "The second cohort disappeared into the space-time continuum. Here's the new list!"
 
-#Let's call the method while deleting :cohort2 in one line.
-puts students.delete(:cohort2)
-# cohort_name_number(students)
-#Except from this point on, :cohort2 is missing...
 
+#Step 7, bonus - Done
 puts "This is the total number of students from all cohorts:"
-add_all_cohorts(students)
+sum_all_cohorts (students)
