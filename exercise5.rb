886d06e2f7873991c46f283d5cc803420fbac344
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-13 15:10:09 -0500

    Exercise5.rb works, prompt to user works and user input is included in method yielding desired result, sentence with string interpolation.

--- exercise5.rb
+++ exercise5.rb
@@ -1,6 +1,6 @@
 puts "Let's convert Fº to Cº! Please give me a temperature in Fahrenheit:"
 
-f = gets.chomp
+f = gets.chomp.to_i
 
 #I think it's going to break here because I have no way of calling the method f_to_c after I've asked the user for input...
 
@@ -9,3 +9,5 @@ def f_to_c(f)
   puts "The Fahrenheit equivalent of #{f} is #{c} degrees Celcius."
   return c
 end
+
+f_to_c(f)
