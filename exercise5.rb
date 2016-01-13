58743b5ef3d243f0bb6127a5752b9f47e07835ec
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-13 15:19:25 -0500

    Exercise5.rb commit, put method before prompt to user and removed parenth around 5 div by 9 to use int instead of float to have rounded celcius answers. Still returning true.

--- exercise5.rb
+++ exercise5.rb
@@ -1,13 +1,15 @@
-puts "Let's convert Fº to Cº! Please give me a temperature in Fahrenheit:"
-
-f = gets.chomp.to_i
+# puts "Let's convert Fº to Cº! Please give me a temperature in Fahrenheit:"
+#
+# f = gets.chomp.to_i
 
 #I think it's going to break here because I have no way of calling the method f_to_c after I've asked the user for input...
 
 def f_to_c(f)
-  c = (f - 32) * (5.0 / 9.0)
-  puts "The Fahrenheit equivalent of #{f} is #{c} degrees Celcius."
-  return c
+  c = (f - 32) * 5 / 9
+  p "The Fahrenheit equivalent of #{f} is #{c} degrees Celcius."
+  # return c
 end
 
+puts "Let's convert Fº to Cº! Please give me a temperature in Fahrenheit:"
+f = gets.chomp.to_i
 f_to_c(f)
