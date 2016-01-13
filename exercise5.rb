a77d2a8a0d725bb597567324117f1fcc9e31affd
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-13 14:47:31 -0500

    exercise5.rb commit number two

--- exercise5.rb
+++ exercise5.rb
@@ -1,6 +1,11 @@
-puts "Let's convert Fº to Cº! Please give me a temperature in Fahrenheit:"
+# puts "Let's convert Fº to Cº! Please give me a temperature in Fahrenheit:"
+#
+# f = gets.chomp.to_i
 
-f_temp = gets.chomp.to_i
+#I think it's going to break here because I have no way of calling the method f_to_c after I've asked the user for input...
 
-def f_to_c(f_temp)
-  (f_temp - 32) * (5/9) = c 
+def f_to_c(f)
+  c = ((f.to_i - 32) * (5/9))
+  puts "The Fahrenheit equivalent of #{f_temp} is #{c} degrees Celcius."
+  return c
+end
