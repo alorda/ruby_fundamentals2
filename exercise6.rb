9011922780ed9ea7fb40596cd27264fa75f49c2c
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-13 16:37:31 -0500

    exercise6.rb added correct first four items and asterisk to lists.

--- exercise6.rb
+++ exercise6.rb
@@ -1,11 +1,11 @@
 def show_me_groceries(list)
-  list.each { |item| puts item }
+  list.each { |item| puts "* " + item }
   puts "\n"
 end
 
-grocery_list = ["carrots", "celery", "humus", "chocolate", "peanuts"]
+grocery_list = ["carrots", "toilet paper", "apples", "salmon", "celery", "humus", "chocolate", "peanuts"]
 
-grocery_list.each { |item| puts item }
+grocery_list.each { |item| puts "* " + item }
 
 puts "I need to add some more items... let's check the list again:\n"
 
@@ -18,3 +18,11 @@ puts "How many items are on the list?"
 puts grocery_list.count
 
 puts grocery_list.include?("bananas") ? "You need to buy bananas" : "You don't need to pick up bananas today."
+
+puts "What's the second item on the list? Be careful with your indeces!"
+
+puts grocery_list[1]
+
+puts "A grocery store that sorts items in alpha order?! How unique! Let's reorder our list then!"
+
+puts grocery_list.sort
