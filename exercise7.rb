79c7119ab0de81826ec431ef6ac4bfb5248412aa
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-13 17:32:49 -0500

    ex 7.rb, fourth kv pair added and grew cohort size by 5 percent and kept it as integers.

--- exercise7.rb
+++ exercise7.rb
@@ -10,4 +10,16 @@ def cohort_name_number(hash)
   end
 end
 
+def cohort_plus_five_percent(hash)
+  hash.each do |cohort, number|
+    puts "#{cohort.capitalize} grew from #{number} students to #{(number * 1.05).to_i} students."
+  end
+end
+
 cohort_name_number(students)
+
+students[:cohort4] = 43
+
+puts "Classrooms have grown by 5%; we have new cohort sizes!"
+
+cohort_plus_five_percent(students)
