fef46561575ed426ec53465ebe2b5a9781dfc70f
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-13 17:51:14 -0500

    Not much changed on exercise5.rb but keeping method at top and the rest at bottom

--- exercise5.rb
+++ exercise5.rb
@@ -4,12 +4,15 @@
 
 #I think it's going to break here because I have no way of calling the method f_to_c after I've asked the user for input...
 
-def f_to_c(f)
-  c = (f - 32) * 5 / 9
-  p "The Fahrenheit equivalent of #{f} is #{c} degrees Celcius."
-  # return c
+def f_to_c(fahr)
+  celcius = (fahr - 32) * 5 / 9
+  puts "The equivalent of #{fahr}º Fahrenheit is #{celcius}º Celcius."
+  return celcius
 end
 
 puts "Let's convert Fº to Cº! Please give me a temperature in Fahrenheit:"
 f = gets.chomp.to_i
 f_to_c(f)
+
+
+#I'm trying to figure out why I can't get the => value to be the numerical celcius value...
