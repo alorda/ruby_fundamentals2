8cbb87d7da8a7dda96384182c11c3331a766b58a
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-13 19:26:36 -0500

    exercise7.rb all methods written but cannot puts students because undef local variable.

--- exercise7.rb
+++ exercise7.rb
@@ -1,4 +1,4 @@
-#This is my hash
+#This is my hash DONE
 
 students = {
   :cohort1 => 34,
@@ -21,23 +21,33 @@ end
 
 def add_a_cohort(hash, key, value)
   hash[key.to_sym] = value
+  # puts students #is this a bad idea?
 end
 
-def sum_all_cohorts (hash)
+def delete_cohort(hash, key)
+  hash.delete(key)
+  # puts students
+end
+
+def sum_all_cohorts(hash)
   total_students = hash.each_value.inject(:+)
   puts total_students
 end
 
+
 #End of Method Definitions: Let's use them!
 
-#Step 2
+#Step 2 DONE
 display_cohort(students)
 
 #Step 3 Add cohort 4 with 43 students
+puts "Let's add a cohort!"
+add_a_cohort(students, "cohort4", 43)
+puts students
 
-#Step 4 - Use Keys method to output cohort names
+#Step 4 - Use Keys method to output cohort names DONE
 puts "These are the names of the cohorts:"
-puts students.keys 
+puts students.keys
 
 
 #Step 5 - Expand Classrooms by 5% - Display - Done
@@ -47,7 +57,8 @@ cohort_plus_five_percent(students)
 
 #Step 6 Delete the second cohort and display
 puts "The second cohort disappeared into the space-time continuum. Here's the new list!"
-
+delete_cohort(students, :cohort2)
+puts students
 
 #Step 7, bonus - Done
 puts "This is the total number of students from all cohorts:"
